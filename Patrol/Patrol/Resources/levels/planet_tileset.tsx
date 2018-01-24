437ac3c4e3925b0a6073e3f5ce4fcb1c4ffe86ca
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2018-01-24 03:15:06 +0200

    Revert "changed map, removed layers, only 1 tileset"
    This reverts commit ff77c98b8394515d9dd4a14489dd43b68fe178b5.

--- Patrol/Patrol/Resources/levels/planet_tileset.tsx
+++ Patrol/Patrol/Resources/levels/planet_tileset.tsx
@@ -2,51 +2,51 @@
 <tileset name="planet" tilewidth="128" tileheight="128" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="128" height="128" source="../PlanetTileset/Tile/1.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/1.png"/>
  </tile>
  <tile id="1">
-  <image width="128" height="128" source="../PlanetTileset/Tile/2.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/2.png"/>
  </tile>
  <tile id="2">
-  <image width="128" height="128" source="../PlanetTileset/Tile/3.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/3.png"/>
  </tile>
  <tile id="3">
-  <image width="128" height="128" source="../PlanetTileset/Tile/4.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/4.png"/>
  </tile>
  <tile id="4">
-  <image width="128" height="128" source="../PlanetTileset/Tile/5.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/5.png"/>
  </tile>
  <tile id="5">
-  <image width="128" height="128" source="../PlanetTileset/Tile/6.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/6.png"/>
  </tile>
  <tile id="6">
-  <image width="128" height="128" source="../PlanetTileset/Tile/7.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/7.png"/>
  </tile>
  <tile id="7">
-  <image width="128" height="128" source="../PlanetTileset/Tile/8.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/8.png"/>
  </tile>
  <tile id="8">
-  <image width="128" height="128" source="../PlanetTileset/Tile/9.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/9.png"/>
  </tile>
  <tile id="9">
-  <image width="128" height="128" source="../PlanetTileset/Tile/10.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/10.png"/>
  </tile>
  <tile id="10">
-  <image width="128" height="128" source="../PlanetTileset/Tile/11.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/11.png"/>
  </tile>
  <tile id="11">
-  <image width="128" height="128" source="../PlanetTileset/Tile/12.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/12.png"/>
  </tile>
  <tile id="12">
-  <image width="128" height="128" source="../PlanetTileset/Tile/13.png"/>
+  <image width="128" height="128" source="../planet_tileset/Tile/13.png"/>
  </tile>
  <tile id="13">
-  <image width="128" height="93" source="../PlanetTileset/Tile/14.png"/>
+  <image width="128" height="93" source="../planet_tileset/Tile/14.png"/>
  </tile>
  <tile id="14">
-  <image width="128" height="93" source="../PlanetTileset/Tile/15.png"/>
+  <image width="128" height="93" source="../planet_tileset/Tile/15.png"/>
  </tile>
  <tile id="15">
-  <image width="128" height="93" source="../PlanetTileset/Tile/16.png"/>
+  <image width="128" height="93" source="../planet_tileset/Tile/16.png"/>
  </tile>
 </tileset>
