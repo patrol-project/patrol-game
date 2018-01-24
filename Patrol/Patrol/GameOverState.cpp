d1bfca6815e409b7ba15b97b120c32e2a6fb0d6e
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2018-01-24 02:57:20 +0200

    Revert to the last state it worked before messing with tilesets.Revert "Updated onEnter and onExit functions of the states. Fixed bug from pause state to main menu state. everything seems to work ok?!"
    This reverts commit d7c0b3ed6ab6846c6fcfb6268762f0b7f1f7c502.

--- Patrol/Patrol/GameOverState.cpp
+++ Patrol/Patrol/GameOverState.cpp
@@ -9,23 +9,6 @@
 
 const std::string GameOverState::gameOverID = "GAMEOVER";
 
-void GameOverState::setCallbacks(const std::vector<Callback>& callbacks)
-{
-	// go through the game objects
-	if (!gameObjects.empty())
-	{
-		for (unsigned int i = 0; i < gameObjects.size(); i++)
-		{
-			// if they are of type MenuButton then assign a callback based on the id passed in from the file
-			if (dynamic_cast<MenuButton*>(gameObjects[i]))
-			{
-				MenuButton* pButton = dynamic_cast<MenuButton*>(gameObjects[i]);
-				pButton->setCallback(callbacks[pButton->getCallbackID()]);
-			}
-		}
-	}
-}
-
 void GameOverState::gameOverToMain()
 {
 	Game::Instance().getStateMachine()->set_next_state(STATE_MAIN_MENU);
@@ -53,41 +36,23 @@ void GameOverState::render()
 
 bool GameOverState::onEnter()
 {
-	// parse the state
-	StateParser stateParser;
-	stateParser.parseState("States.xml", gameOverID, &gameObjects, &m_textureIDList);
-	m_callbacks.push_back(0);
-	m_callbacks.push_back(gameOverToMain);
-	m_callbacks.push_back(restartPlay);
-	// set the callbacks for menu items
-	setCallbacks(m_callbacks);
-	std::cout << "entering GameOverState\n";
 	return true;
 }
 
 bool GameOverState::onExit()
 {
-	m_exiting = true;
-
-	// clean the game objects
-	if (m_loadingComplete && !gameObjects.empty())
+	for (int i = 0; i < gameObjects.size(); i++)
 	{
-		gameObjects.back()->clean();
-		gameObjects.pop_back();
+		gameObjects[i]->clean();
 	}
 
 	gameObjects.clear();
 
-
-	// clear the texture manager
-	for (unsigned int i = 0; i < m_textureIDList.size(); i++)
-	{
-		TextureManager::Instance()->clearFromTextureMap(m_textureIDList[i]);
-	}
-
-	// reset the input handler
+	TextureManager::Instance()->clearFromTextureMap("gameovertext");
+	TextureManager::Instance()->clearFromTextureMap("mainbutton");
+	TextureManager::Instance()->clearFromTextureMap("restartbutton");
+	// reset the mouse button states to false
 	InputHandler::Instance()->reset();
 
-	std::cout << "exiting GameOverState\n";
 	return true;
 }
